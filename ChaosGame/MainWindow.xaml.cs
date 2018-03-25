a61518a902dcabfd0344dad60f3758a2bda73ca1
Anne <annemedekker@gmail.com>
2018-03-26 00:02:14 +0200

    Minimal working state
    The program is capable of generating and rendering chaos points (if only that)

--- ChaosGame/MainWindow.xaml.cs
+++ ChaosGame/MainWindow.xaml.cs
@@ -15,14 +15,52 @@ using System.Windows.Shapes;
 
 namespace ChaosGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        WriteableBitmap wbmp;
+        Image wbmpImage;
+        ChaosCode chaos;
+
+        // color settings
+
+        List<Color> cornerColors = new List<Color>() { Colors.Aqua, Colors.Gold, Colors.Lime };
+
+        // generation settings
+
+        int generateManyCount = 15000;
+
+        // init
+
         public MainWindow()
         {
             InitializeComponent();
+
+            chaos = new ChaosCode();
+            wbmp = new WriteableBitmap(chaos.RenderSize, chaos.RenderSize, 128, 128, PixelFormats.Bgra32, null);
+            wbmpImage = new Image();
+            wbmpImage.Source = wbmp;
+            wbmpImage.Width = wbmpImage.Height = 1000;
+            theCanvas.Children.Add(wbmpImage);
+        }
+
+        // drawing chaos points
+
+        private void DrawPoints(List<XYC> points)
+        {
+            foreach (XYC p in points)
+            {
+                wbmp.SetPixel(p.x, p.y,
+                    cornerColors[p.c % cornerColors.Count]);
+            }
+        }
+
+        // master controls
+
+        private void GenerateMany_Click(object sender, RoutedEventArgs e)
+        {
+            List<XYC> points = chaos.GetChaosPoints(generateManyCount);
+
+            DrawPoints(points);
         }
     }
 }
